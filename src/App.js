967d0d7bd5870e5d8893cee84e3e1c83656aac16
R-Joshua-Sujith <joshuasujith14@gmail.com>
2023-07-15 07:46:53 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,158 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const itemsPerPage = 10;
+const pagesToShow = 10;
 function App() {
+  const [data, setEmojiData] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [selectedCategory, setSelectedCategory] = useState('All');
+  const [category, setCategory] = useState([]);
+
+  useEffect(() => {
+    const getData = async () => {
+      await axios.get("https://emojihub.yurace.pro/api/all")
+        .then((res) => {
+          setEmojiData(res.data);
+
+          const arr = res.data.map((item) => item.category);
+          arr.unshift('All');
+          const set = new Set(arr);
+          const newArr = Array.from(set);
+          setCategory(newArr);
+        })
+    }
+    getData();
+
+  }, [])
+
+
+  const indexOfLastItem = currentPage * itemsPerPage;
+  const indexOfFirstItem = indexOfLastItem - itemsPerPage;
+  const filteredData = selectedCategory === 'All' ? data : data.filter((item) => item.category === selectedCategory);
+  const totalItems = filteredData.length;
+  const currentItems = filteredData.slice(indexOfFirstItem, indexOfLastItem);
+  const handleCategoryChange = (event) => {
+    setSelectedCategory(event.target.value);
+
+  };
+  const handlePageChange = (pageNumber) => {
+    setCurrentPage(pageNumber);
+  };
+
+
+  const pageNumbers = [];
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  if (currentPage <= pagesToShow - 3) {
+    for (let i = 1; i <= Math.min(totalPages, pagesToShow); i++) {
+      pageNumbers.push(
+        <button key={i} onClick={() => handlePageChange(i)}>
+          {i}
+        </button>
+      );
+    }
+    if (totalPages > pagesToShow) {
+      pageNumbers.push(
+        <span key="ellipsis">...</span>
+      );
+    }
+    pageNumbers.push(
+      <button key={totalPages} onClick={() => handlePageChange(totalPages)}>
+        {totalPages}
+      </button>
+    );
+  } else if (currentPage >= totalPages - (pagesToShow - 2)) {
+    pageNumbers.push(
+      <button key={1} onClick={() => handlePageChange(1)}>
+        1
+      </button>
+    );
+    if (totalPages > pagesToShow) {
+      pageNumbers.push(
+        <span key="ellipsis">...</span>
+      );
+    }
+    for (let i = totalPages - (pagesToShow - 2); i <= totalPages; i++) {
+      pageNumbers.push(
+        <button key={i} onClick={() => handlePageChange(i)}>
+          {i}
+        </button>
+      );
+    }
+  } else {
+    pageNumbers.push(
+      <button key={1} onClick={() => handlePageChange(1)}>
+        1
+      </button>
+    );
+    if (totalPages > pagesToShow) {
+      pageNumbers.push(
+        <span key="ellipsis">...</span>
+      );
+    }
+    for (let i = currentPage - Math.floor((pagesToShow - 3) / 2); i <= currentPage + Math.floor((pagesToShow - 3) / 2); i++) {
+      pageNumbers.push(
+        <button key={i} onClick={() => handlePageChange(i)}>
+          {i}
+        </button>
+      );
+    }
+    if (totalPages > pagesToShow) {
+      pageNumbers.push(
+        <span key="ellipsis">...</span>
+      );
+    }
+    pageNumbers.push(
+      <button key={totalPages} onClick={() => handlePageChange(totalPages)}>
+        {totalPages}
+      </button>
+    );
+  }
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <div className="filter">
+        <select
+          className="filter-Select"
+          value={selectedCategory}
+
+          onChange={handleCategoryChange}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          {category.map((option, i) => (
+            <option key={i} value={option}>
+              {option}
+            </option>
+          ))}
+
+        </select>
+      </div>
+
+      <div className='emojiList'>
+        {
+          currentItems.map((emoji) => (
+            <div className='emoji'>
+              <div className='icon'> <span className='icon' dangerouslySetInnerHTML={{ __html: emoji.htmlCode[0] }}></span></div>
+              <div class="hover-content">
+                <div>Name: {emoji.name}</div>
+                <div>Category: {emoji.category}</div>
+                <div>Group: {emoji.group}</div>
+              </div>
+            </div>
+          ))
+        }
+
+
+      </div>
+      <div className='buttons'>
+        {pageNumbers.map((number) => (
+          <span className='button' key={number}>{number}</span>
+        ))}
+      </div>
+    </div >
   );
 }
 
---
+++ src/data.js
@@ -0,0 +1,270 @@
+const data = [
+    {
+        name: 'ghost',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128123;'],
+        unicode: ['U+1F47B']
+    },
+    {
+        name: 'extraterrestrial alien ≊ alien',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128125;'],
+        unicode: ['U+1F47D']
+    },
+    {
+        name: 'alien monster',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128126;'],
+        unicode: ['U+1F47E']
+    },
+    {
+        name: 'robot face',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#129302;'],
+        unicode: ['U+1F916']
+    },
+    {
+        name: 'pile of poo',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128169;'],
+        unicode: ['U+1F4A9']
+    },
+    {
+        name: 'smiling cat face with open mouth',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128570;'],
+        unicode: ['U+1F63A']
+    },
+    {
+        name: 'grinning cat face with smiling eyes',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128568;'],
+        unicode: ['U+1F638']
+    },
+    {
+        name: 'cat face with tears of joy',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128569;'],
+        unicode: ['U+1F639']
+    },
+    {
+        name: 'smiling cat face with heart-shaped eyes ≊ smiling cat face with heart-eyes',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128571;'],
+        unicode: ['U+1F63B']
+    },
+    {
+        name: 'cat face with wry smile',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128572;'],
+        unicode: ['U+1F63C']
+    },
+    {
+        name: 'kissing cat face with closed eyes',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128573;'],
+        unicode: ['U+1F63D']
+    },
+    {
+        name: 'weary cat face',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128576;'],
+        unicode: ['U+1F640']
+    },
+    {
+        name: 'crying cat face',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128575;'],
+        unicode: ['U+1F63F']
+    },
+    {
+        name: 'pouting cat face',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128574;'],
+        unicode: ['U+1F63E']
+    },
+    {
+        name: 'see-no-evil monkey ≊ see-no-evil',
+        category: 'smileys and people',
+        group: 'monkey face',
+        htmlCode: ['&#128584;'],
+        unicode: ['U+1F648']
+    },
+    {
+        name: 'hear-no-evil monkey ≊ hear-no-evil',
+        category: 'smileys and people',
+        group: 'monkey face',
+        htmlCode: ['&#128585;'],
+        unicode: ['U+1F649']
+    },
+    {
+        name: 'speak-no-evil monkey ≊ speak-no-evil',
+        category: 'smileys and people',
+        group: 'monkey face',
+        htmlCode: ['&#128586;'],
+        unicode: ['U+1F64A']
+    },
+    {
+        name: 'boy',
+        category: 'smileys and people',
+        group: 'person',
+        htmlCode: ['&#128102;'],
+        unicode: ['U+1F466']
+    },
+    {
+        name: 'boy, type-1-2',
+        category: 'smileys and people',
+        group: 'person',
+        htmlCode: ['&#128102;', '&#127995;'],
+        unicode: ['U+1F466', 'U+1F3FB']
+    },
+    {
+        name: 'ghost',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128123;'],
+        unicode: ['U+1F47B']
+    },
+    {
+        name: 'extraterrestrial alien ≊ alien',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128125;'],
+        unicode: ['U+1F47D']
+    },
+    {
+        name: 'alien monster',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128126;'],
+        unicode: ['U+1F47E']
+    },
+    {
+        name: 'robot face',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#129302;'],
+        unicode: ['U+1F916']
+    },
+    {
+        name: 'pile of poo',
+        category: 'smileys and people',
+        group: 'creature face',
+        htmlCode: ['&#128169;'],
+        unicode: ['U+1F4A9']
+    },
+    {
+        name: 'smiling cat face with open mouth',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128570;'],
+        unicode: ['U+1F63A']
+    },
+    {
+        name: 'grinning cat face with smiling eyes',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128568;'],
+        unicode: ['U+1F638']
+    },
+    {
+        name: 'cat face with tears of joy',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128569;'],
+        unicode: ['U+1F639']
+    },
+    {
+        name: 'smiling cat face with heart-shaped eyes ≊ smiling cat face with heart-eyes',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128571;'],
+        unicode: ['U+1F63B']
+    },
+    {
+        name: 'cat face with wry smile',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128572;'],
+        unicode: ['U+1F63C']
+    },
+    {
+        name: 'kissing cat face with closed eyes',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128573;'],
+        unicode: ['U+1F63D']
+    },
+    {
+        name: 'weary cat face',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128576;'],
+        unicode: ['U+1F640']
+    },
+    {
+        name: 'crying cat face',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128575;'],
+        unicode: ['U+1F63F']
+    },
+    {
+        name: 'pouting cat face',
+        category: 'smileys and people',
+        group: 'cat face',
+        htmlCode: ['&#128574;'],
+        unicode: ['U+1F63E']
+    },
+    {
+        name: 'see-no-evil monkey ≊ see-no-evil',
+        category: 'smileys and people',
+        group: 'monkey face',
+        htmlCode: ['&#128584;'],
+        unicode: ['U+1F648']
+    },
+    {
+        name: 'hear-no-evil monkey ≊ hear-no-evil',
+        category: 'smileys and people',
+        group: 'monkey face',
+        htmlCode: ['&#128585;'],
+        unicode: ['U+1F649']
+    },
+    {
+        name: 'speak-no-evil monkey ≊ speak-no-evil',
+        category: 'smileys and people',
+        group: 'monkey face',
+        htmlCode: ['&#128586;'],
+        unicode: ['U+1F64A']
+    },
+    {
+        name: 'boy',
+        category: 'smileys and people',
+        group: 'person',
+        htmlCode: ['&#128102;'],
+        unicode: ['U+1F466']
+    },
+    {
+        name: 'boy, type-1-2',
+        category: 'smileys and people',
+        group: 'person',
+        htmlCode: ['&#128102;', '&#127995;'],
+        unicode: ['U+1F466', 'U+1F3FB']
+    },
+]
+
+export default data;
